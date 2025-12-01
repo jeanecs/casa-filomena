cc6dd4aa4bff07aef768f79d17f574534093e867
mako2jz <roobiksangelo@gmail.com>
2025-12-01 13:30:20 +0800

    changed img sources and readme

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ export function Hero() {
     <section className="relative h-screen flex items-center justify-center overflow-hidden">
       <div className="absolute inset-0">
         <ImageWithFallback
-          src="https://scontent.fcgy1-1.fna.fbcdn.net/v/t39.30808-6/561601915_122106831063028391_138386498224499306_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=102&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=62dXsxPN4dIQ7kNvwGPYwTw&_nc_oc=AdlTDv1Dy4IF0gmR4jLIw3nRbDc7lQP4LNA1LScSMmYBUOCsr4JHuk7IfaKVsWPSd7I&_nc_zt=23&_nc_ht=scontent.fcgy1-1.fna&_nc_gid=lif4ySTN1padc86vhnh5AQ&oh=00_Afgsc79j4wcU3-jAEN1blNp3XiYNyvL4XpgvkmkGCMQDpw&oe=692C7187"
+          src="images\561601915_122106831063028391_138386498224499306_n.jpg"
           alt="Luxury beach villa with ocean view"
           className="w-full h-full object-cover blur-[2px]"
         />
--- src/components/VillaPortal.tsx
+++ src/components/VillaPortal.tsx
@@ -6,7 +6,7 @@ export function VillaPortal() {
     <div className="relative w-full max-w-[1280px] h-[200px] mx-auto">
       {/* Background Image */}
       <ImageWithFallback
-        src="https://scontent.fcgy2-2.fna.fbcdn.net/v/t39.30808-6/581025896_122112331239028391_1986668517674544899_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=103&ccb=1-7&_nc_sid=833d8c&_nc_ohc=HYJ4aU5r36AQ7kNvwGz-T7Q&_nc_oc=AdltNaGnL5jRrgeKY7shtAcUvEahUealYQLmrEYcoxfHIt2BXZE66_c5X2-FFqcr5HQ&_nc_zt=23&_nc_ht=scontent.fcgy2-2.fna&_nc_gid=x7vMcAu9PnABKNDDU28-ww&oh=00_AfgaYi1yeOh1G14iOUdmx_r1FDmuyuZf1SUQzz4luB5A6w&oe=692CC514"
+        src="images\581025896_122112331239028391_1986668517674544899_n.jpg"
         alt="Villa Portal"
         className="w-full h-full object-cover object-center filter brightness-[0.7] rounded-md"
       />
