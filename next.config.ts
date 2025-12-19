16d78a5712fa7b1cdcf7c451c09502d061a6a500
jeanecs <jeane.eritch@gmail.com>
2025-12-20 03:37:00 +0800

    fix: clean up next.config.ts and update package.json scripts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 };
 
 export default nextConfig;
